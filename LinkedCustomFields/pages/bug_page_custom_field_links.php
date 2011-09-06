edf3b298fdff851281a7e1d9493ce898f8bc18f4
Robert Munteanu <robert@lmn.ro>
2011-09-07 00:01:17 +0300

    Implement actual linked selection functionality
    Still missing:
    
    - ensure that key and values are properly encoded/escaped
    - selection of field value after options refresh

--- LinkedCustomFields/pages/bug_page_custom_field_links.php
+++ LinkedCustomFields/pages/bug_page_custom_field_links.php
@@ -15,6 +15,8 @@ $t_all_custom_field_ids = custom_field_get_linked_ids( $t_project_id );
 
 ?>
 var linkedFieldValues = {};
+var allFieldValues = {};
+var bindings = {};
 <?php
 
 foreach ( $t_all_custom_field_ids as $t_custom_field_id ) {
@@ -24,16 +26,60 @@ foreach ( $t_all_custom_field_ids as $t_custom_field_id ) {
     }
     
     $t_linked_values = LinkedCustomFieldsDao::getLinkedValuesMap( $t_custom_field_id );
+    
     if ( count ( $t_linked_values ) > 0 ) {
+        
+        // values from $t_custom_field_id trigger filter values from $t_linked_field_id 
+        
         $t_linked_field_id = LinkedCustomFieldsDao::getLinkedFieldId( $t_custom_field_id );
         echo JavascriptUtils::consoleLog('Found linked field with id '. $t_custom_field_id . ' , linked to ' . $t_linked_field_id);
         
-        echo 'linkedFieldValues['.$t_linked_field_id.'] = [];';
+        $t_linked_field = custom_field_get_definition( $t_linked_field_id );
+        
+        echo 'bindings["' . $t_custom_field_id.'"] = "'. $t_linked_field_id.'";'."\n";
+        echo 'allFieldValues["' .$t_custom_field_id.'"] = ' . JavascriptUtils::toJSArray( explode('|', $t_linked_field['possible_values']) ).";\n";
+        echo 'linkedFieldValues["'.$t_custom_field_id."\"] = {};\n";
         
         foreach ( $t_linked_values as $t_source_value => $t_target_values ) {
-            echo 'linkedFieldValues['.$t_linked_field_id.']['. $t_source_value.'] = ' . JavascriptUtils::toJSArray( $t_target_values ).";\n";
+            echo 'linkedFieldValues["'.$t_custom_field_id.'"]["'. $t_source_value.'"] = ' . JavascriptUtils::toJSArray( $t_target_values ).";\n";
         }
     }
 }
-
 ?>
+
+var refreshLinkedValues = function(fieldId, fieldValue) {
+    var targetValues = linkedFieldValues[fieldId][fieldValue];
+    
+    if ( ! targetValues ) {
+        targetValues = allFieldValues[fieldId] ;
+    }
+    
+    var targetFieldRef = jQuery('[name=custom_field_' + bindings[fieldId] +']'); 
+    
+    targetFieldRef.empty();
+    for ( var i = 0 ; i < targetValues.length; i++ ) {
+        var targetValue = targetValues[i];
+        console.info("Appending target value " + targetValue);
+        targetFieldRef.append(jQuery('<option/>').
+            attr('value', targetValue).
+            text(targetValue));
+    }
+    
+    console.info("Legal values should be " + fieldValue + " -> " + targetValues);
+};
+
+jQuery(document).ready(function() {
+    for ( boundKey in bindings ) {
+        console.info("Bound " + boundKey +" -> " + bindings[boundKey]);
+        
+        var applicable = linkedFieldValues[boundKey];
+        
+        var customFieldRef = jQuery('[name=custom_field_'+boundKey+']'); 
+        
+        refreshLinkedValues(boundKey, customFieldRef.val());
+        
+        customFieldRef.change(function() {
+            refreshLinkedValues(boundKey, jQuery(this).val());
+        });
+    }
+});
