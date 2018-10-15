707d5c02483228d52e8c0480ed60cb3ca23d76bb
Damien Regad <dregad@mantisbt.org>
2018-10-15 13:31:18 +0200

    Add REST route to retrieve custom field values

--- LinkedCustomFields/LinkedCustomFields.php
+++ LinkedCustomFields/LinkedCustomFields.php
@@ -35,6 +35,7 @@ class LinkedCustomFieldsPlugin extends MantisPlugin {
 		return array(
 			'EVENT_MENU_MANAGE' => 'manage_custom_field_links',
 			'EVENT_LAYOUT_RESOURCES' => 'resources',
+			'EVENT_REST_API_ROUTES' => 'routes',
 		);
 	}
 
@@ -73,4 +74,71 @@ class LinkedCustomFieldsPlugin extends MantisPlugin {
 			)
 		);
 	}
+
+	/**
+	 * Add the RESTful routes handled by this plugin.
+	 *
+	 * @param string $p_event_name The event name
+	 * @param array  $p_event_args The event arguments
+	 * @return void
+	 */
+	public function routes( $p_event_name, $p_event_args ) {
+		$t_app = $p_event_args['app'];
+		$t_plugin = $this;
+		$t_app->group(
+			plugin_route_group(),
+			function() use ( $t_app, $t_plugin ) {
+				$t_app->get( '/values/{field_id}', [$t_plugin, 'route_values'] );
+			}
+		);
+	}
+
+	/**
+	 * RESTful route for Custom Fields values.
+	 *
+	 * Returned JSON structure:
+	 * - {array}      List of possible values for given custom field
+	 *
+	 * @param Slim\Http\Request $request
+	 * @param Slim\Http\Response $response
+	 * @param array $args [bug_id = Bug Id for patterns replacement]
+	 * @return Slim\Http\Response
+	 */
+	public function route_values( $request, $response, $args ) {
+		# Set the reference Bug Id for placeholders replacements
+		if( isset( $args['field_id'] ) ) {
+			$t_field_id = (int)$args['field_id'];
+		} else {
+			$t_field_id = 0;
+		}
+
+		# Retrieve Custom Field definition
+		$t_custom_field_def = custom_field_cache_row( $t_field_id, false );
+
+		# Make sure Custom Field exists and its type is supported
+		if( !$t_custom_field_def ) {
+			return $response->withStatus(
+				HTTP_STATUS_BAD_REQUEST,
+				"Invalid Custom Field Id"
+			);
+		}
+		switch( $t_custom_field_def['type'] ) {
+			case CUSTOM_FIELD_TYPE_ENUM:
+			case CUSTOM_FIELD_TYPE_MULTILIST:
+				break;
+			default:
+				return $response->withStatus(
+					HTTP_STATUS_BAD_REQUEST,
+					"Unsupported Custom Field Type"
+				);
+		}
+
+		# Return possible values
+		return $response
+			->withStatus( HTTP_STATUS_SUCCESS )
+			->withJson(
+				explode( '|', $t_custom_field_def['possible_values'] )
+			);
+	}
+
 }
