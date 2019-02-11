060c2dddda5ce4aafd40c8666cf015bab670d495
stevecharon <37655537+stevecharon@users.noreply.github.com>
2019-02-11 09:20:35 +0100

    Files from #24226

---
+++ LinkedCustomFields.php
@@ -0,0 +1,81 @@
+<?php
+# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+
+# Linked custom fields for MantisBT is free software: 
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, 
+# either version 2 of the License, or (at your option) any later version.
+#
+# Linked custom fields plugin for MantisBT is distributed in the hope 
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Linked custom fields plugin for MantisBT.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+class LinkedCustomFieldsPlugin extends MantisPlugin {
+    
+    public function register() {
+        $this->name = plugin_lang_get("title");
+        $this->description = plugin_lang_get("description");
+
+        $this->version = "2.0.0";
+
+        $this->requires = array(
+ 			"MantisCore" => "2.0.0, <2.30.0",
+        );
+
+        $this->author = "Robert Munteanu";
+        $this->contact = "robert@lmn.ro";
+        $this->url ="http://www.mantisbt.org/wiki/doku.php/mantisbt:linkedcustomfields";
+    }
+    
+    public function hooks() {
+        return array(
+            'EVENT_MENU_MANAGE' => 'manage_custom_field_links',
+            'EVENT_LAYOUT_RESOURCES' => 'resources',
+        );
+    }
+    
+    public function manage_custom_field_links( $p_is_admin ) {
+        
+        return array( '<a href="' . plugin_page( 'configure_custom_field_links' ) . '">' . plugin_lang_get( 'configure_custom_field_links' ) . '</a>', );
+    }
+    
+    function resources( $p_event ) {
+        
+        $t_bug_id = gpc_get_int('bug_id', -1);
+        $t_m_id = gpc_get_int('m_id', 0);
+        if ( $t_bug_id == -1 && basename($_SERVER['SCRIPT_NAME']) == 'bug_report_page.php' ) {
+            $t_bug_id = 0;
+        }
+        if ( $t_bug_id != -1 ) {
+            return '<script type="text/javascript" src="' . plugin_page( 'bug_page_custom_field_links.php' ) . '&amp;bug_id='. $t_bug_id .'&amp;m_id='.$t_m_id.'"></script>';
+        }
+    }    
+    
+    public function init() {
+        
+        require_once 'LinkedCustomFields.API.php';
+    }
+    
+    public function schema() {
+        return array(
+            array( 'CreateTableSQL', 
+                array( plugin_table( 'data' ), "
+                    custom_field_id    I NOTNULL,
+                    custom_field_value_order    I NOTNULL,
+                    custom_field_value C(255) NOTNULL DEFAULT \" '' \",
+                    target_field_id    I NOTNULL,
+                    target_field_values    C(255) NOTNULL DEFAULT \" '' \"
+                "),
+            ),
+        	array( 'AlterColumnSQL',
+        		array( plugin_table( 'data' ), " custom_field_value XL, target_field_values XL")
+        	)
+        );
+    }
+}
+?>
---
+++ pages/configure_custom_field_link_update.php
@@ -0,0 +1,52 @@
+<?php
+# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+
+# Linked custom fields for MantisBT is free software:
+# you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation,
+# either version 2 of the License, or (at your option) any later version.
+#
+# Linked custom fields plugin for MantisBT is distributed in the hope
+# that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Linked custom fields plugin for MantisBT.
+# If not, see <http://www.gnu.org/licenses/>.
+
+	require_once( 'core.php' );
+
+	access_ensure_global_level( config_get( 'manage_custom_fields_threshold' ) );
+	
+	form_security_validate( 'configure_custom_field_link' );
+	
+	$t_value_mappings = array();
+	
+	$f_source_field_id =  gpc_get_int('custom_field_id');
+	$t_source_field = custom_field_get_definition( $f_source_field_id );
+	$t_source_field_values = explode ( '|', $t_source_field['possible_values']);
+	
+	$f_target_field_id = gpc_get_int('target_custom_field');
+	
+	foreach ( $_POST as $f_post_key => $f_post_value ) {
+	    
+	    if ( strpos($f_post_key, 'custom_field_linked_values_') === 0 ) {
+	        
+	        $t_source_value_index = substr( $f_post_key, strlen ('custom_field_linked_values_') );
+	        $t_source_value = $t_source_field_values[ $t_source_value_index ];
+	        $t_linked_value = gpc_get( $f_post_key );
+            $t_value_mappings[$t_source_value] = $t_linked_value;
+	    }
+	}
+
+	form_security_purge( 'configure_custom_field_link' );
+	
+	if ( LinkedCustomFieldsDao::getLinkedFieldId( gpc_get_int('target_custom_field') ) ) {
+	    // plugin_get_current('target_field_already_linked')
+	    trigger_error( ERROR_GENERIC , ERROR );
+	} else {
+    	LinkedCustomFieldsDao::replaceValues( $f_source_field_id, $f_target_field_id , $t_value_mappings);
+        header("Location: " . plugin_page('configure_custom_field_links.php'));
+	}
+?>
