5718fc9d12443a1eaa3bba3e655fd79a3cc80b95
Damien Regad <dregad@mantisbt.org>
2022-02-18 18:40:32 +0100

    Update file header and copyright

--- LinkedCustomFields/pages/link_edit.php
+++ LinkedCustomFields/pages/link_edit.php
@@ -1,21 +1,27 @@
 <?php
-# Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+/**
+ * Linked custom fields plugin for MantisBT
+ *
+ * Copyright (c) 2011 Robert Munteanu (robert@lmn.ro)
+ * Copyright (c) 2018, 2022 Damien Regad
+ *
+ * Linked custom fields for MantisBT is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation,
+ * either version 2 of the License, or (at your option) any later version.
+ *
+ * Linked custom fields plugin for MantisBT is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Linked custom fields plugin for MantisBT.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @noinspection PhpUnhandledExceptionInspection
+ */
 
-# Linked custom fields for MantisBT is free software:
-# you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation,
-# either version 2 of the License, or (at your option) any later version.
-#
-# Linked custom fields plugin for MantisBT is distributed in the hope
-# that it will be useful, but WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Linked custom fields plugin for MantisBT.
-# If not, see <http://www.gnu.org/licenses/>.
-
-/** @noinspection PhpUnhandledExceptionInspection */
 auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_custom_fields_threshold' ) );
 
